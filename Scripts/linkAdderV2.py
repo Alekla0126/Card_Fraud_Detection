9880c3a54c0cb3784f3ea498e98b96afd5827a0b
alekla0126 <alekla0126@gmail.com>
2023-05-03 12:13:57 +0300

    Course work V3.0.1

--- Scripts/linkAdderV2.py
+++ Scripts/linkAdderV2.py
@@ -9,77 +9,77 @@ import os
 def is_printable(s):
     return all(c in string.printable for c in s)
 
-def add_links(transactions_file, links_file, output_file, threshold):
-    print("Reading links CSV file...")
-    # Read the links CSV file and extract the good and bad links
-    links = defaultdict(list)
-    with open(links_file, 'r') as f:
+def add_urls(transactions_file, urls_file, output_file, threshold):
+    print("Reading urls CSV file...")
+    # Read the urls CSV file and extract the good and bad urls
+    urls = defaultdict(list)
+    with open(urls_file, 'r') as f:
         reader = csv.DictReader(f)
         for row in reader:
-            links[row['Label']].append(row['URL'])
+            urls[row['Label']].append(row['URL'])
 
-    # Drop rows containing non-printable characters from the links lists
+    # Drop rows containing non-printable characters from the urls lists
     print('Drop rows containing non-printable characters...')
-    links = {label: [link for link in links[label] if is_printable(link)] for label in links}
+    urls = {label: [url for url in urls[label] if is_printable(url)] for label in urls}
 
     print("Processing transactions...")
-    num_good_links = 0
-    num_bad_links = 0
-    num_incorrect_bad_links = 0
-    num_incorrect_good_links = 0
+    num_good_urls = 0
+    num_bad_urls = 0
+    num_incorrect_bad_urls = 0
+    num_incorrect_good_urls = 0
     # Process the transactions using multiple threads
     with open(output_file, 'w', newline='') as f_out, open(transactions_file, 'r') as f_in:
         reader = csv.DictReader(f_in)
-        fieldnames = reader.fieldnames + ['Link']
+        fieldnames = reader.fieldnames + ['url']
         writer = csv.DictWriter(f_out, fieldnames=fieldnames)
         writer.writeheader()
 
         # Instantiating the lists
-        good_links = links['good']
-        bad_links = links['bad']
+        good_urls = urls['good']
+        bad_urls = urls['bad']
         # Loop through rows
         i_good = 0
         i_bad = 0
         for i, row in enumerate(reader):
             print(f"Processing transaction {i}...")
-            # Get the link for this row
-            link = None
+            # Get the url for this row
+            url = None
             if int(row['is_fraud']) == 1:
                 if random.random()+.00521 <= threshold:                          
-                    if i_good == len(links['good']):
+                    if i_good == len(urls['good']):
                         i_good = 0
-                    link = good_links[i_good]
-                    num_incorrect_good_links += 1
+                    url = good_urls[i_good]
+                    num_incorrect_good_urls += 1
                     i_good += 1
                 else:
-                    if i_bad >= len(bad_links):
+                    if i_bad >= len(bad_urls):
                         i_bad = 0
-                    link = bad_links[i_bad]
-                    num_bad_links += 1
+                    url = bad_urls[i_bad]
+                    num_bad_urls += 1
                     i_bad += 1
             elif int(row['is_fraud']) == 0:
                 if random.random()+.995 <= threshold:
-                    if i_bad >= len(bad_links):
+                    if i_bad >= len(bad_urls):
                         i_bad = 0
-                    link = bad_links[i_bad]
-                    num_incorrect_bad_links += 1
+                    url = bad_urls[i_bad]
+                    num_incorrect_bad_urls += 1
                     i_bad += 1
                 else:
-                    if i_good >= len(links['good']):
+                    if i_good >= len(urls['good']):
                         i_good = 0
-                    link = good_links[i_good]
-                    num_good_links +=1
+                    url = good_urls[i_good]
+                    num_good_urls +=1
                     i_good += 1
 
-            # Update the links column
-            row['Link'] = link
+            # Update the urls column
+            row['url'] = url
             writer.writerow(row)
 
     print("Finished processing transactions.")
-    print(f"Number of good links: {num_good_links}")
-    print(f"Number of bad links: {num_bad_links}")
-    print(f"Number of incorrectly paired good links: {num_incorrect_good_links}")
-    print(f"Number of incorrectly paired bad links: {num_incorrect_bad_links}")
+    print(f"Number of good urls: {num_good_urls}")
+    print(f"Number of bad urls: {num_bad_urls}")
+    print(f"Number of incorrectly paired good urls: {num_incorrect_good_urls}")
+    print(f"Number of incorrectly paired bad urls: {num_incorrect_bad_urls}")
     # Play a sound to indicate that the process is complete.
     os.system('afplay /System/Library/Sounds/Glass.aiff')
     time.sleep(5)
@@ -87,21 +87,21 @@ def add_links(transactions_file, links_file, output_file, threshold):
     os.system('say "Process complete, bro"')
 
 def main():
-    parser = argparse.ArgumentParser(description='Add links to transactions')
+    parser = argparse.ArgumentParser(description='Add urls to transactions')
     parser.add_argument('transactions_file', type=str, help='Path to the transactions CSV file')
-    parser.add_argument('links_file', type=str, help='Path to the links CSV file')
+    parser.add_argument('urls_file', type=str, help='Path to the urls CSV file')
     parser.add_argument('output_file', type=str, help='Path to the output CSV file')
-    parser.add_argument('threshold', type=float, help='Probability threshold for adding bad links')
+    parser.add_argument('threshold', type=float, help='Probability threshold for adding bad urls')
     args = parser.parse_args()
 
-    add_links(args.transactions_file, args.links_file, args.output_file, args.threshold)
+    add_urls(args.transactions_file, args.urls_file, args.output_file, args.threshold)
 
 
 if __name__ == '__main__':
     main()
 
 # Example usage:
-# add_links('transactions.csv', 'links.csv', 'output.csv', 1, 4)
+# add_urls('transactions.csv', 'urls.csv', 'output.csv', 1, 4)
 
 # Example input CSV:
 # Card Number,Zipcode,Amount,is_fraud
@@ -110,7 +110,7 @@ if __name__ == '__main__':
 # 345678901,34567,300,1
 
 # Example output CSV:
-# Card Number,Zipcode,Amount,is_fraud,Link
-# 123456789,12345,100,0,https://good-link.com
-# 234567890,23456,200,0,https://good-link.com
-# 345678901,34567,300,1,https://bad-link.com
+# Card Number,Zipcode,Amount,is_fraud,url
+# 123456789,12345,100,0,https://good-url.com
+# 234567890,23456,200,0,https://good-url.com
+# 345678901,34567,300,1,https://bad-url.com
---
+++ ccfd/api.py
@@ -0,0 +1,71 @@
+from sklearn.preprocessing import OrdinalEncoder, StandardScaler
+from sklearn.feature_extraction.text import CountVectorizer
+from flask import Flask, jsonify, request, render_template
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import RegexpTokenizer
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+import requests
+
+# Load the trained model
+model = tf.keras.models.load_model('LSTM.h5')
+
+# Initialize the tokenizer, stemmer, and vectorizer.
+tokenizer = RegexpTokenizer(r'[A-Za-z]+')
+stemmer = SnowballStemmer('english')
+cv = CountVectorizer()
+
+# Initialize the ordinal encoder and scaler
+enc = OrdinalEncoder(dtype=np.int64)
+scaler = StandardScaler()
+
+# Load the phishing URLs from OpenPhish and cache them.
+url = 'https://openphish.com/feed.txt'
+openphish_urls = set(requests.get(url).text.strip().split('\n'))
+
+app = Flask(__name__)
+
+# Define the home page route.
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get the input data.
+    input_data = request.form.to_dict()
+    
+    # Preprocess the input data.
+    input_df = pd.DataFrame({'url': [input_data['url']]})
+    input_df['category'] = input_data.get('category', '')
+    input_df['merchant'] = input_data.get('merchant', '')
+    input_df['job'] = input_data.get('job', '')
+    input_df['label'] = 0
+    
+    # Tokenize and stem the text.
+    input_df['text_tokenized'] = input_df.url.map(lambda t: tokenizer.tokenize(t))
+    input_df['text_stemmed'] = input_df.text_tokenized.map(lambda t: [stemmer.stem(word) for word in t])
+    input_df['text_encoded'] = input_df.text_stemmed.map(lambda t: [cv.vocabulary_.get(word, -1) + 1 for word in t])
+    input_df['text_padded'] = tf.keras.preprocessing.sequence.pad_sequences(input_df['text_encoded'], maxlen=100)
+    # Encode the categorical features.
+    input_df['category_encoded'] = enc.fit_transform(input_df[['category']])
+    input_df['merchant_encoded'] = enc.fit_transform(input_df[['merchant']])
+    input_df['job_encoded'] = enc.fit_transform(input_df[['job']])
+
+    # Scale the numerical features.
+    numerical_features = ['category_encoded', 'merchant_encoded', 'job_encoded']
+    input_df[numerical_features] = scaler.fit_transform(input_df[numerical_features])
+
+    # Make the prediction.
+    prediction = model.predict(input_df[['category_encoded', 'merchant_encoded', 'job_encoded', 'text_padded']])
+    prediction_class = np.argmax(prediction)
+
+    # Check if the URL is in the OpenPhish dataset.
+    in_openphish = input_data['url'] in openphish_urls
+
+    # Return the prediction and whether the URL is in the OpenPhish dataset.
+    return jsonify({'prediction': prediction_class, 'in_openphish': in_openphish})
+
+if __name__ == '__main__':
+    app.run()
