d72cf98e70a8ffc7e664ec7a9813c4ba26086a14
alekla0126 <alekla0126@gmail.com>
2023-05-04 01:55:24 +0300

    First try to deploy

--- ccfd/api.py
+++ ccfd/api.py
@@ -33,39 +33,52 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    # Get the input data.
-    input_data = request.form.to_dict()
-    
-    # Preprocess the input data.
-    input_df = pd.DataFrame({'url': [input_data['url']]})
-    input_df['category'] = input_data.get('category', '')
-    input_df['merchant'] = input_data.get('merchant', '')
-    input_df['job'] = input_data.get('job', '')
-    input_df['label'] = 0
-    
-    # Tokenize and stem the text.
-    input_df['text_tokenized'] = input_df.url.map(lambda t: tokenizer.tokenize(t))
-    input_df['text_stemmed'] = input_df.text_tokenized.map(lambda t: [stemmer.stem(word) for word in t])
-    input_df['text_encoded'] = input_df.text_stemmed.map(lambda t: [cv.vocabulary_.get(word, -1) + 1 for word in t])
-    input_df['text_padded'] = tf.keras.preprocessing.sequence.pad_sequences(input_df['text_encoded'], maxlen=100)
-    # Encode the categorical features.
-    input_df['category_encoded'] = enc.fit_transform(input_df[['category']])
-    input_df['merchant_encoded'] = enc.fit_transform(input_df[['merchant']])
-    input_df['job_encoded'] = enc.fit_transform(input_df[['job']])
+    try:
+        # Get the input data.
+        input_data = request.form.to_dict()
+        
+        # Preprocess the input data.
+        input_df = pd.DataFrame({'url': [input_data['url']]})
+        input_df['category'] = input_data.get('category', '')
+        input_df['merchant'] = input_data.get('merchant', '')
+        input_df['job'] = input_data.get('job', '')
+        input_df['label'] = 0
+        
+        # Tokenize and stem the text.
+        input_df['text_tokenized'] = input_df.url.map(lambda t: tokenizer.tokenize(t))
+        input_df['text_stemmed'] = input_df.text_tokenized.map(lambda t: [stemmer.stem(word) for word in t])
+        input_df['text_encoded'] = input_df.text_stemmed.map(lambda t: [cv.vocabulary_.get(word, -1) + 1 for word in t])
+        input_df['text_padded'] = tf.keras.preprocessing.sequence.pad_sequences(input_df['text_encoded'], maxlen=100)
+        
+        # Convert form values to float
+        input_data['category'] = float(input_data.get('category', ''))
+        input_data['merchant'] = float(input_data.get('merchant', ''))
+        input_data['job'] = float(input_data.get('job', ''))
+        
+        # Encode the categorical features.
+        input_df['category_encoded'] = enc.fit_transform(input_df[['category']])
+        input_df['merchant_encoded'] = enc.fit_transform(input_df[['merchant']])
+        input_df['job_encoded'] = enc.fit_transform(input_df[['job']])
 
-    # Scale the numerical features.
-    numerical_features = ['category_encoded', 'merchant_encoded', 'job_encoded']
-    input_df[numerical_features] = scaler.fit_transform(input_df[numerical_features])
+        # Scale the numerical features.
+        numerical_features = ['category_encoded', 'merchant_encoded', 'job_encoded']
+        input_df[numerical_features] = scaler.fit_transform(input_df[numerical_features])
 
-    # Make the prediction.
-    prediction = model.predict(input_df[['category_encoded', 'merchant_encoded', 'job_encoded', 'text_padded']])
-    prediction_class = np.argmax(prediction)
+        # Make the prediction.
+        prediction = model.predict(input_df[['category_encoded', 'merchant_encoded', 'job_encoded', 'text_padded']])
+        prediction_class = np.argmax(prediction)
 
-    # Check if the URL is in the OpenPhish dataset.
-    in_openphish = input_data['url'] in openphish_urls
+        # Check if the URL is in the OpenPhish dataset.
+        in_openphish = input_data['url'] in openphish_urls
 
-    # Return the prediction and whether the URL is in the OpenPhish dataset.
-    return jsonify({'prediction': prediction_class, 'in_openphish': in_openphish})
+        # Return the prediction and whether the URL is in the OpenPhish dataset.
+        return jsonify({'prediction': prediction_class, 'in_openphish': in_openphish})
+
+    except Exception as e:
+            # Handle the exception and return an error message
+            error_message = f"An error occurred: {str(e)}"
+            print(traceback.format_exc())  # Print the error traceback to the console
+            return jsonify({'status': 'error', 'message': error_message})
 
 if __name__ == '__main__':
     app.run()
