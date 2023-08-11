3748863b0ffe4f741b346d4ecf39dd4f0e24725d
alekla0126 <alekla0126@gmail.com>
2023-08-11 20:06:54 +0300

    3.0.8

--- Scripts/Download_dataset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -O https://www.kaggle.com/datasets/kartik2112/fraud-detection/download?datasetVersionNumber=1
-unzip archive.zip
-mv archive Datasets
-awk 'FNR==1 && NR!=1{next;}{print}' *.csv > Datasets/master.csv
-rm -rf fraudTest.csv && rm -rf fraudTrain.csv
