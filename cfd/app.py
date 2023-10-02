92a215de9a89c9f42c197b8865578c380bce569d
alekla0126 <alekla0126@gmail.com>
2023-10-02 13:06:15 +0300

    V2.0.14

--- cfd/app.py
+++ cfd/app.py
@@ -11,4 +11,4 @@ with app.app_context():
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(host='0.0.0.0', port=port)
